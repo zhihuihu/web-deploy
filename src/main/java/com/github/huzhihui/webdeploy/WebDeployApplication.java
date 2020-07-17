539306308ccd7f531c8df7f62be089e1d01734ad
huzhihui <huzhihui_c@qq.com>
2020-07-17 13:57:36 +0800

    init

---
+++ src/main/java/com/github/huzhihui/webdeploy/WebDeployApplication.java
@@ -0,0 +1,17 @@
+package com.github.huzhihui.webdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * web前端发布系统
+ * @author huzhihui
+ */
+@SpringBootApplication
+public class WebDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/huzhihui/webdeploy/WebDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.huzhihui.webdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
